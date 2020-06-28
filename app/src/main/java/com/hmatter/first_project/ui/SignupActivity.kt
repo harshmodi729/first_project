21462672908dfabd74b6400a5714c7682bb080fb
Richa Shah <richashah117@gmail.com>
2020-06-28 12:27:52 +0530

    Sign-up added

--- app/src/main/java/com/hmatter/first_project/ui/SignupActivity.kt
+++ app/src/main/java/com/hmatter/first_project/ui/SignupActivity.kt
@@ -12,7 +12,7 @@ class SignupActivity : AppCompatActivity() {
         setContentView(R.layout.activity_signup)
 
         btnToolbarBack.visibility = View.GONE
-        btnToolbarBack.visibility = View.GONE
+
 
 
     }
