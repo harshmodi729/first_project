a7789fdd50738f384cd08cdab35a701e98a8757b
Richa Shah <richashah117@gmail.com>
2020-07-05 23:52:44 +0530

    Download screen added

---
+++ app/src/main/java/com/hmatter/first_project/adapter/DownloadAdapter.kt
@@ -0,0 +1,38 @@
+package com.hmatter.first_project.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hmatter.first_project.R
+import kotlinx.android.synthetic.main.lay_row_downloads.view.*
+
+class DownloadAdapter(private val items: MutableList<String>) : RecyclerView.Adapter<DownloadAdapter.VH>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        return VH(parent)
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+       holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    fun addItem(name: String) {
+        items.add(name)
+        notifyItemInserted(items.size)
+    }
+
+    fun removeAt(position: Int) {
+        items.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+    class VH(parent: ViewGroup) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.lay_row_downloads, parent, false)) {
+
+        fun bind(name: String) = with(itemView) {
+            tvClassName.text = name
+        }
+    }
+}
