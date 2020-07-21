e6b389b8175698ba025561e0e8e516d2faa517b6
harsh <harshmodi729@gmail.com>
2020-07-22 02:00:21 +0530

    Add list checkbox for videoCategories

--- app/src/main/java/com/hmatter/first_project/remote/ApiServices.kt
+++ app/src/main/java/com/hmatter/first_project/remote/ApiServices.kt
@@ -1,6 +1,7 @@
 package com.hmatter.first_project.remote
 
 import com.hmatter.first_project.model.PopularClassItem
+import com.hmatter.first_project.model.VideoCategoryItem
 import retrofit2.http.GET
 
 interface ApiServices {
@@ -8,4 +9,7 @@ interface ApiServices {
     @GET("9756db14-2aa0-44dd-8bc9-30629ea66ddd")
     suspend fun getPopularClasses()
             : ApiResponse<ArrayList<PopularClassItem>>
+
+    @GET("8aa3d6ee-bb06-46ea-8614-1f49846ce2de")
+    suspend fun getVideoCategory(): ApiResponse<ArrayList<VideoCategoryItem>>
 }
--- app/src/main/java/com/hmatter/first_project/ui/fragment/SearchFragment.kt
+++ app/src/main/java/com/hmatter/first_project/ui/fragment/SearchFragment.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hmatter.first_project.R
 import com.hmatter.first_project.adapter.PopularTagsAdapter
+import com.hmatter.first_project.adapter.VideoCategoryAdapter
 import com.hmatter.first_project.base.BaseFragment
+import com.hmatter.first_project.base.BaseResult
+import com.hmatter.first_project.extension.makeToast
+import com.hmatter.first_project.viewmodel.SearchViewModel
 import kotlinx.android.synthetic.main.fragment_search.*
 import kotlinx.android.synthetic.main.lay_toolbar.*
 
 class SearchFragment : BaseFragment(R.layout.fragment_search) {
-    val tagList: ArrayList<String> = ArrayList()
+    private val tagList: ArrayList<String> = ArrayList()
+    private val alSelectedCategory = ArrayList<Int>()
+    private lateinit var videoCategoryAdapter: VideoCategoryAdapter
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
@@ -20,6 +29,31 @@ class SearchFragment : BaseFragment(R.layout.fragment_search) {
         tvToolbarTitle.text = getString(R.string.search)
         tvToolbarTitle.visibility = View.GONE
         ivSearchLayout.visibility = View.VISIBLE
+
+        videoCategoryAdapter = VideoCategoryAdapter(mContext)
+        rvVideoCategory.adapter = videoCategoryAdapter
+        val searchViewModel = ViewModelProviders.of(this)[SearchViewModel::class.java]
+        searchViewModel.getVideoCategories().apply {
+            laySearch.post {
+                showProgressDialog(laySearch, ivDialogBg)
+            }
+        }
+        searchViewModel.alVideoCategoryItem.observe(
+            viewLifecycleOwner,
+            Observer {
+                hideProgressDialog(ivDialogBg)
+                when (it) {
+                    is BaseResult.Success -> {
+                        videoCategoryAdapter.addData(it.item)
+                        rvVideoCategory.visibility = View.VISIBLE
+                        tvTagView.visibility = View.GONE
+                        rvTag.visibility = View.GONE
+                    }
+                    is BaseResult.Error -> {
+                        mContext.makeToast(it.errorMessage)
+                    }
+                }
+            })
 
         val cooking = resources.getStringArray(R.array.cooking_array)
         // Create the adapter and set it to the AutoCompleteTextView
@@ -35,15 +69,22 @@ class SearchFragment : BaseFragment(R.layout.fragment_search) {
 
         tagList.add("Business & Management")
         tagList.add("Creative Art & Media")
-        tagList.add("Health & Psycology")
+        tagList.add("Health & Psychology")
         tagList.add("History")
         tagList.add("Language and Culture")
         tagList.add("Science Engineering & Maths")
         tagList.add("Study Skills")
         tagList.add("Tech & Coding")
 
-        var layoutManager = LinearLayoutManager(activity)
-        rcvTag.layoutManager = layoutManager
-        rcvTag.adapter = activity?.let { PopularTagsAdapter(it, tagList) }
+        val layoutManager = LinearLayoutManager(mContext)
+        rvTag.layoutManager = layoutManager
+        rvTag.adapter = PopularTagsAdapter(mContext, tagList)
+
+        videoCategoryAdapter.onCategorySelectedListener = { item, isChecked ->
+            if (isChecked)
+                alSelectedCategory.add(item.categoryId)
+            else
+                alSelectedCategory.remove(item.categoryId)
+        }
     }
 }
