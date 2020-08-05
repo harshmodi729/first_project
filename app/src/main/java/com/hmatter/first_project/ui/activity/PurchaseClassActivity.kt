d7161c0d8f407ec5715c8095dd12cf1674c44e1d
Richa Shah <richashah117@gmail.com>
2020-08-06 02:07:27 +0530

    Purchase-Detail added

---
+++ app/src/main/java/com/hmatter/first_project/ui/activity/PurchaseClassActivity.kt
@@ -0,0 +1,12 @@
+package com.hmatter.first_project.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hmatter.first_project.R
+
+class PurchaseClassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_purchase_class)
+    }
+}
